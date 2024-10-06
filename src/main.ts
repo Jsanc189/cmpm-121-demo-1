77f7b5f8c855d8cbe667c7827eae18d8b46bab03
Jsanc189 <146498952+Jsanc189@users.noreply.github.com>
2024-10-06 00:44:26 -0700

    Added button element

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-
 const button = document.createElement("button");
 button.innerHTML = "Click me!";
 app.append(button);
