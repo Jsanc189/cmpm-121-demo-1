afc902cc9b7037dca3d73a952159885155125498
Jsanc189 <146498952+Jsanc189@users.noreply.github.com>
2024-10-10 23:40:38 -0700

    implemented Command structure for future buttons

--- src/main.ts
+++ src/main.ts
@@ -13,53 +13,101 @@ const pieCounterDiv = document.createElement("div");
 pieCounterDiv.innerHTML = `Total Pies: 0`;
 app.append(pieCounterDiv);
 
-//set up autoclicker to only work when a user clicks once
-requestAnimationFrame(addPies);
 
-let lastTime = 0;
 let total_pies: number = 0;
 const button = document.createElement("button");
 button.innerHTML = "Click me! 🥧";
 button.addEventListener("click", () => {
-  incrementPies(true);
+  incrementPies(true, 0);
 });
 app.append(button);
 
-let growth_rate: number = 0;
-const upgrade = document.createElement("button");
-upgrade.innerHTML = `Auto ${growth_rate + 1}X 🥧\nCost: 10 🥧`;
-upgrade.addEventListener("click", () => {
-  if (
-    total_pies / (growth_rate * 10) >= 1 &&
-    total_pies >= (growth_rate + 1) * 10
-  ) {
-    total_pies = total_pies - (growth_rate + 1) * 10;
-    incrementPies();
-    growth_rate = growth_rate + 1;
-  }
-});
-app.append(upgrade);
+let growth_rate: number = 1;
 
-function addPies(timestamp: number) {
-  if (
-    (timestamp - lastTime) / 1000 > 1 &&
-    total_pies != 0 &&
-    growth_rate != 0
-  ) {
-    incrementPies();
-    lastTime = timestamp;
-  }
-  requestAnimationFrame(addPies);
+type executeFunction = () => boolean; // This function will represent the action of the upgrade.
+
+
+class upgradeCommand{
+    private readonly executeFunction: executeFunction;
+    public cost: number;
+    public name: string;
+    public rate: number;
+    public newRate: number;
+
+    constructor(executeFunction: executeFunction, cost: number, name: string, rate: number, newRate: number) {
+        this.executeFunction = executeFunction;
+        this.cost = cost;
+        this.name = name;
+        this.rate = rate;
+        this.newRate = newRate;
+    }
+
+    execute(): boolean {
+        if (total_pies >= this.cost) {
+            total_pies -= this.cost;
+            this.executeFunction();
+            this.rate += this.rate;
+            this.cost *= this.rate;
+            if (this.rate > 0) {
+                this.enableAutoClicker();
+            }
+            this.name = `Increase Growth Rate to ${(this.rate)}X`
+            return true;
+        }
+        return false;
+    }
+
+    getDetails() : string {
+        return `${this.name} \nCost: ${this.cost} 🥧`;
+    }
+
+    enableAutoClicker() {
+        let lastTime = 0;
+        const autoclick = (timestamp: number) => {
+            if ((timestamp - lastTime) >= 1000 ) {
+                total_pies += this.rate;
+                pieCounterDiv.innerHTML = `Total Pies: ${total_pies}`;
+                 
+                lastTime = timestamp;
+                }
+            requestAnimationFrame(autoclick);
+            };
+        requestAnimationFrame(autoclick);
+    }
 }
 
+const upgradeCommand1 = new upgradeCommand(
+    () => {
+        growth_rate = 1;
+        return true;
+    },
+    10,
+    `Increase Growth Rate to ${growth_rate}X`,
+    1,
+    0
+);
+
+function setUpgradeButton(button: HTMLButtonElement, command: upgradeCommand) {
+    button.innerHTML = command.getDetails();
+    button.addEventListener("click", () => {
+        if (command.execute()) {
+            button.innerHTML = command.getDetails();
+            pieCounterDiv.innerHTML = `Total Pies: ${total_pies}`;
+        }
+     });
+     app.append(button);
+}
+
+const upgradeButton1 = document.createElement("button");
+setUpgradeButton(upgradeButton1, upgradeCommand1);
+
 //increment pies by 1 or by growth rate.
-function incrementPies(isClicked: boolean = false) {
+function incrementPies(isClicked: boolean = false, growth_rate: number) {
   if (isClicked || growth_rate === 0) {
     total_pies = total_pies + 1;
   } else {
-    total_pies = total_pies + growth_rate * 1;
+    total_pies = total_pies + growth_rate;
   }
   pieCounterDiv.innerHTML = `Total Pies: ${total_pies} 🥧`;
-  upgrade.innerHTML = `${growth_rate + 1}X 🥧\nCost: ${(growth_rate + 1) * 10} 🥧`;
   button.innerHTML = "Click me! 🥧";
 }
